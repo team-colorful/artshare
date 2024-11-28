362130fe5cbd71419b979a476bf00203dc97ee9b
suzukaotto <me@szk.kr>
2024-11-28 18:11:20 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+import os
+import dotenv
+from flask import Flask, request, jsonify, render_template
+
+dotenv.load_dotenv()
+
+HOST_IP    =  str(os.getenv('HOST_IP'))
+HOST_PORT  =  int(os.getenv('HOST_PORT'))
+HOST_DEBUG = bool(os.getenv('HOST_DEBUG'))
+
+app = Flask(__name__)
+
+if __name__ == '__main__':
+    app.run(port=os.getenv('PORT'), debug=True)
