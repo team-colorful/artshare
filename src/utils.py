6751c4837841bdbf668a1fec24ba13bbdc3d318a
suzukaotto <me@szk.kr>
2024-11-29 00:26:46 +0000

    update: code update

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,16 @@
+import datetime
 import os
 import hashlib
+
+default_timef = "%Y%m%d%H%M%S"
+
+def get_now_ftime(time_format: str | None = default_timef) -> str:
+    time = datetime.now()
+    f_time = time.strftime(time_format)
+    return f_time
+
+def convert_now_ftime(_time_str: str, _format = '%Y%m%d%H%M%S') -> datetime:
+    return datetime.strptime(_time_str, _format)
 
 def get_hash(_data = os.urandom(32)):
     return hashlib.sha256(_data).hexdigest()
